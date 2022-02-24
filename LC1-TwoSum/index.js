2de4e463e49431d8600a10194969507b88fb654d
SURAJKP <surajkpatil444@gmail.com>
2022-02-24 22:23:10 +0530

    time complexity O(n)

--- LC1-TwoSum/index.js
+++ LC1-TwoSum/index.js
@@ -1,11 +1,23 @@
 function twoSum(arr, t) {
+    // for(let i=0; i<arr.length; i++) {
+    //     for(let j=i+1; i<arr.length; j++) {
+    //       if(arr[i] + arr[j] === t) {
+    //     return [i, j];
+    //     }
+    //   }
+    // } time complexity O(n^2)
+    const numVisited = {}; // {2:0, 7:1}
+    const res = [];
     for(let i=0; i<arr.length; i++) {
-        for(let j=i+1; i<arr.length; j++) {
-          if(arr[i] + arr[j] === t) {
-        return [i, j];
+        const num = arr[i];
+        const complement = t-num;
+        if(numVisited[complement] !== undefined) {
+            res.push(i);
+            res.push(numVisited[complement])
         }
-      }
+        numVisited[num] = i;
     }
-}
+    return res; //time complexity O(n)
+} 
 
 module.exports = twoSum;
